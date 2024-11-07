29b9552e7bda190e69ee9aea889f5bf70249dd29
Douglas Hindson <dmhindson@gmail.com>
2024-11-07 16:46:12 +0000

    Fix Dockerfiles.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,16 @@
 # api/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .crew.main import extract_beliefs, write_parable
+#from .crew.main import extract_beliefs, write_parable
 
 class ExtractBeliefsView(APIView):
     def post(self, request):
         text = request.data.get('text')
         author_background = request.data.get('author_background')
 
-        beliefs = extract_beliefs(text, author_background).to_dict()['beliefs']
+        #beliefs = extract_beliefs(text, author_background).to_dict()['beliefs']
 
-        return Response({'beliefs': beliefs})
+        return Response({'beliefs': []})
 
 class GenerateParableView(APIView):
     def post(self, request):
@@ -20,6 +20,6 @@ class GenerateParableView(APIView):
         context = request.data.get('author_background')
         assert action in ["changed", "reinforced", "meaningless"]
 
-        parable = write_parable(belief, action, text, context)
+        #parable = write_parable(belief, action, text, context)
 
-        return Response({'parable': str(parable)})
+        return Response({'parable': ""})
