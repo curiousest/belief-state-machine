095d6ac23d3da07ef396aad70cd9962ec2e930f2
Douglas Hindson <dmhindson@gmail.com>
2024-11-14 16:19:53 +0000

    Use terraform state bucket.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "bsm-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "bsm-terraform-lock-table"
+    encrypt        = true
+  }
+}
