54f1e46ff04ecd2c1222fb7f4996d60b9ffb6d7c
Douglas Hindson <dmhindson@gmail.com>
2024-11-14 19:37:00 +0000

    Fix.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,18 @@ output "backend_service_name" {
   description = "Name of the backend ECS service"
   value       = aws_ecs_service.backend.name
 }
+
+output "frontend_ecr_repository_url" {
+  description = "URL of the frontend ECR repository"
+  value       = aws_ecr_repository.frontend.repository_url
+}
+
+output "backend_ecr_repository_url" {
+  description = "URL of the backend ECR repository"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
